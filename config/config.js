905ce87ff5bcc4be3b9b4746d86f338e49ceb146
Christo <>
2023-08-04 14:52:17 +0530

    envs

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const myEnv = require('dotenv').config()
-const ADMIN_EMAIL=process.env.EMAIL
-const APP_PASSWORD=process.env.APP_PASSWORD
-const PORT=process.env.PORT
-const RazorID=process.env.RAZORPAY_ID_KEY
-const razorpass=process.env.RAZORPAY_SECRET_KEY
+const ADMIN_EMAIL='christovarghese555@gmail.com'
+const APP_PASSWORD='lawsgrhqfjvsvqnq'
+const PORT=5453
+const RazorID='rzp_test_Aguu49rE1TW4wR'
+const razorpass='Hipe3z6MvsEly76OBPUCRRrr'
 
 module.exports={ADMIN_EMAIL,
                 APP_PASSWORD,
